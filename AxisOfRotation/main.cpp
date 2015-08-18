7da385262f39011a4d7f4b4512338031b09f35e2
masakazu nakazawa <masakazu@nakazawa.local>
2015-08-18 14:06:17 +0900

    Initial Commit

---
+++ AxisOfRotation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AxisOfRotation
+//
+//  Created by masakazu nakazawa on 2015/08/18.
+//  Copyright (c) 2015年 masakazu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
